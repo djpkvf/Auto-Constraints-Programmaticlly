ab0200e1bf4d0b6b911ccdcbdeb30ae21c86135e
Dominic <djpkvf@mail.missouri.edu>
2018-06-29 18:10:50 -0500

    Added layout constraints for UIImageView and UILabel

--- Auto Constraints Programmatically/Auto Constraints Programmatically/ViewController.swift
+++ Auto Constraints Programmatically/Auto Constraints Programmatically/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let wineBottleImageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "wine-bottles"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let imageTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Wine Bottles"
+        label.font = UIFont(name: "Helvetica", size: 18)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(wineBottleImageView)
+        view.addSubview(imageTitleLabel)
+        
+        setupViews()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func setupViews() {
+        wineBottleImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        wineBottleImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        wineBottleImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        wineBottleImageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        wineBottleImageView.contentMode = .scaleAspectFit
+        
+        imageTitleLabel.topAnchor.constraint(equalTo: wineBottleImageView.bottomAnchor, constant: 0).isActive = true
+        imageTitleLabel.leftAnchor.constraint(equalTo: wineBottleImageView.leftAnchor).isActive = true
+        imageTitleLabel.rightAnchor.constraint(equalTo: wineBottleImageView.rightAnchor).isActive = true
+        imageTitleLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Auto-Constraints-Programmaticlly
 INFOTC 4001 iOS Level 2 Course at Mizzou
+
+A YouTube channel I've been a fan of for a while has some great videos on this subject:
+1. https://www.youtube.com/watch?v=9RydRg0ZKaI
+2. https://www.youtube.com/watch?v=nv1L3mYEg8M
